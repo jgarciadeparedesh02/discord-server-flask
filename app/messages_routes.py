5f7cc736941ce728bbe0c910601ab594069c3793
jgarciadeparedesh02 <jgarciadeparedesh02@educarex.es>
2023-11-30 15:49:17 +0100

    messages added

--- app/messages_routes.py
+++ app/messages_routes.py
@@ -22,3 +22,29 @@ def get_chat_messages(chat_id):
     conn.close()
 
     return jsonify(chat_messages)
+
+@app.route('/chats/<int:chat_id>/messages', methods=['POST'])
+def add_message(chat_id):
+    data = request.get_json()
+
+    id_sender = data.get('idSender')
+    content = data.get('content')
+
+    conn = connect()
+    with conn.cursor() as cursor:
+        # Consulta SQL para insertar un nuevo mensaje
+        sql_insert_message = """
+            INSERT INTO tellmedam_message (idSender, content, chatId)
+            VALUES (%s, %s, %s) RETURNING *;
+        """
+        cursor.execute(sql_insert_message, (id_sender, content, chat_id))
+        created_message = cursor.fetchone()
+
+    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        print(type(created_message))
+        cursor.execute('SELECT id, idSender, content, chatId FROM tellmedam_message WHERE chatId = %s and idSender = %s and content = %s;', (chat_id, id_sender, content))
+        created_message = cursor.fetchone()
+    conn.commit()
+    conn.close()
+
+    return jsonify(created_message)
