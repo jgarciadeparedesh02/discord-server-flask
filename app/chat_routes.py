87fdbd214686ea31c6123e1e59bf8c961638c779
jgarciadeparedesh02 <jgarciadeparedesh02@educarex.es>
2023-12-06 13:49:54 +0100

    fixed error in chats create

--- app/chat_routes.py
+++ app/chat_routes.py
@@ -30,6 +30,8 @@ def create_chat():
     new_chat = request.get_json()
     conn = connect()
 
+    print('Try new_chat: ', new_chat)
+
     if 'user1_id' not in new_chat or 'user2_id' not in new_chat:
         return jsonify({'error': 'Campos incompletos'}), 400
     if new_chat['user1_id'] == new_chat['user2_id']:
@@ -47,7 +49,14 @@ def create_chat():
         created_chat = cursor.fetchone()
 
     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-        cursor.execute('SELECT id, idUser1, idUser2 FROM tellmedam_chat WHERE idUser1 = %s AND idUser2 = %s;', (new_chat['user1_id'], new_chat['user2_id']))
+        query = '''
+            SELECT tc.id, tu1.id as user1_id, tu2.id as user2_id, tu1.username as user1_username, tu2.username as user2_username, tc.createdAt, tc.updatedAt
+            FROM tellmedam_chat tc
+            INNER JOIN tellmedam_user tu1 ON tc.idUser1 = tu1.id
+            INNER JOIN tellmedam_user tu2 ON tc.idUser2 = tu2.id
+            WHERE tu1.id = %s AND tu2.id = %s;
+        '''
+        cursor.execute(query, (new_chat['user1_id'], new_chat['user2_id']))
         created_chat = cursor.fetchone()
 
     conn.commit()
